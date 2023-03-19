6c541183ce3ce9ce9bb7b881176e52a59077e4ec
Alvaro Luis Ruiz Alcantara <aruiz-al@c3r7s6.42madrid.com>
2023-03-19 14:51:41 +0100

    busqueda de caracteres nulos y otros caracteres

--- str/ft_strchr.c
+++ str/ft_strchr.c
@@ -6,17 +6,26 @@
 /*   By: aruiz-al <aruiz-al@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/17 15:44:43 by aruiz-al          #+#    #+#             */
-/*   Updated: 2023/03/17 15:59:51 by aruiz-al         ###   ########.fr       */
+/*   Updated: 2023/03/19 14:48:14 by aruiz-al         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strchr(const char *s, int c)
 {
-	while (*s)
+	int	i;
+
+	i = 0;
+	if (c == '\0')
 	{
-		if ((int) s == c)
-			return ((char *) s);
-		++s;
+		while (s[i] != '\0')
+			++i;
+		return ((char *) &s[i]);
+	}
+	while (s[i])
+	{
+		if ((int) s[i] == c)
+			return ((char *) &s[i]);
+		++i;
 	}
 	return (0);
 }
