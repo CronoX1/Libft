4ec6d57cf85f26d7b34f6bca4d0c7fe9c8055cd9
CronoX1 <alvarito9945@gmail.com>
2023-03-24 14:50:36 +0100

    Añadidas las pruebas para strlcpy

--- test/test.c
+++ test/test.c
@@ -5,8 +5,8 @@
 
 int	main(void)
 {
-	char s1[50] = "ho\0la";
-	char s2[50] = "ho\0la";
+	char s1[50] = "";
+	char s2[50] = "";
 	int tab[7] = {-49, 49, 1, -1, 0, -2, 2};
 	char s[] = {0, 1, 2 ,3 ,4 ,5};
 
@@ -19,8 +19,10 @@ int	main(void)
 	printf("strchr vale %s\n", strchr("Hola como estas", 'e'));
 	printf("ft_strrchr vale %s\n", ft_strrchr("Hola como estas", '\0'));
 	printf("strrchr vale %s\n", strrchr("Hola como estas", '\0'));
-	printf("ft_strlcpy vale %lu\n", ft_strlcpy(s1, "", 15));
-	printf("strlcpy vale %li\n", strlcpy(s2, "", 15));
+	printf("ft_strlcpy vale %lu\n", ft_strlcpy(s1, "lorem ipsum", 3));
+	printf("s1 vale %s\n", s1);
+	printf("strlcpy vale %li\n", strlcpy(s2, "lorem ipsum", 3));
+	printf("s2 vale %s\n", s2);
 	printf("memcpy vale %p\n", memcpy(s2, "Ho\200la", 4));
 	printf("ft_memcpy vale %p\n", ft_memcpy(s1, "ho\200la", 4));
 	memcpy(s2, "Hola como estas", 15);
