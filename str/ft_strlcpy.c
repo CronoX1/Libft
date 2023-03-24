80850e1923f879792072fd640a138ecd4c74a991
CronoX1 <alvarito9945@gmail.com>
2023-03-24 14:49:22 +0100

    Pasa todos los tests menos uno

--- str/ft_strlcpy.c
+++ str/ft_strlcpy.c
@@ -3,33 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aruiz-al <aruiz-al@student.42madrid>       +#+  +:+       +#+        */
+/*   By: aruiz-al <aruiz-al@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/19 16:56:18 by aruiz-al          #+#    #+#             */
-/*   Updated: 2023/03/23 13:37:53 by aruiz-al         ###   ########.fr       */
+/*   Updated: 2023/03/24 14:45:48 by aruiz-al         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+size_t	ft_strlcpy(char *dest, const char *src, size_t dstsize)
 {
 	int	i;
 
 	i = 0;
-	if (src[i] == '\0')
-		return (0);
-	if ((int)dstsize - 1 > 0)
+	while ((int)dstsize - 1 > i && src[i])
 	{
-		while (src[i] != '\0' && (int)dstsize - 1 > 0)
-		{
-			dst[i] = src[i];
-			++i;
-			--dstsize;
-		}
-		dst[i] = '\0';
+		dest[i] = src[i];
+		++i;
 	}
-	if (ft_strlen(src) > dstsize)
-		return (ft_strlen(src));
-	return (i);
+	if ((int)dstsize > 0)
+		dest[i] = '\0';
+	return (ft_strlen(src));
 }
