88e8922866070bad2690a8a790f8cee86a4f0c6b
CronoX1 <alvarito9945@gmail.com>
2023-03-28 16:18:27 +0200

    Paco y norminette pasados

---
+++ etc/ft_calloc.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aruiz-al <aruiz-al@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/28 15:41:08 by aruiz-al          #+#    #+#             */
+/*   Updated: 2023/03/28 16:15:40 by aruiz-al         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft.h"
+
+void	*ft_calloc(size_t num, size_t size)
+{
+	int	*ptr;
+
+	ptr = malloc(num * size);
+	if (ptr == NULL)
+		return(NULL);
+	return(ft_bzero(ptr, num * size));
+}
