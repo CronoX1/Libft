8768c54439c32232b64a9f7aed2a02a5eda8bc69
CronoX1 <alvarito9945@gmail.com>
2023-04-19 16:22:19 +0200

    ahora pasa norminette

--- str/ft_strjoin.c
+++ str/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: aruiz-al <aruiz-al@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 14:31:39 by aruiz-al          #+#    #+#             */
-/*   Updated: 2023/04/12 14:59:14 by aruiz-al         ###   ########.fr       */
+/*   Updated: 2023/04/19 15:24:27 by aruiz-al         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	if (final_string == NULL)
 		return (NULL);
 	i = 0;
-	final_string = ft_memcpy((void *) final_string, (const void *) s1, (size_t) ft_strlen(s1));
+	final_string = ft_memcpy(final_string, s1, ft_strlen(s1));
 	while (s2[i])
 	{
 		final_string[ft_strlen(s1) + i] = s2[i];
